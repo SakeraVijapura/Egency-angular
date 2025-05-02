7910766c876c7c6eba331086c1c4708797f295c0
SakeraVijapura <vijapurasakeraben@gmail.com>
2025-05-02 17:21:43 +0530

    import file changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,25 +3,25 @@ import { StatisticsComponent } from '../../components/statistics/statistics.comp
 import { HeroSectionComponent } from '../../components/hero-section/hero-section.component';
 import { BannerSectionComponent } from '../../components/banner-section/banner-section.component';
 import { HowWorkComponent } from '../../components/how-work/how-work.component';
-import { FeaturesContentComponent } from "../../features-section/features-content/features-content.component";
-import { FeaturesCardsComponent } from "../../features-section/features-cards/features-cards.component";
-import { ServiceContentComponent } from "../../components/service-section/service-content/service-content.component";
-import { ServiceCardsComponent } from "../../components/service-section/service-cards/service-cards.component";
-import { GetStartedComponent } from "../../components/get-started/get-started.component";
-import { EventsComponent } from "../../components/events/events.component";
-import { OurTeamComponent } from "../../components/our-team/our-team.component";
-import { FounderWordsComponent } from "../../components/founder-words/founder-words.component";
-import { TestimonialsComponent } from "../../components/testimonials/testimonials.component";
-import { FaqComponent } from "../../components/faq/faq.component";
-import { AwardsComponent } from "../../components/awards/awards.component";
-import { OurBlogComponent } from "../../components/our-blog/our-blog.component";
-import { NewsletterComponent } from "../../components/newsletter/newsletter.component";
-import { GetInTouchComponent } from "../../components/get-in-touch/get-in-touch.component";
-import { ContactSectionComponent } from "../../components/contact-section/contact-section.component";
-import { DreamProjectComponent } from "../../components/dream-project/dream-project.component";
+import { ServiceContentComponent } from '../../components/service-section/service-content/service-content.component';
+import { ServiceCardsComponent } from '../../components/service-section/service-cards/service-cards.component';
+import { GetStartedComponent } from '../../components/get-started/get-started.component';
+import { EventsComponent } from '../../components/events/events.component';
+import { OurTeamComponent } from '../../components/our-team/our-team.component';
+import { FounderWordsComponent } from '../../components/founder-words/founder-words.component';
+import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
+import { FaqComponent } from '../../components/faq/faq.component';
+import { AwardsComponent } from '../../components/awards/awards.component';
+import { OurBlogComponent } from '../../components/our-blog/our-blog.component';
+import { NewsletterComponent } from '../../components/newsletter/newsletter.component';
+import { GetInTouchComponent } from '../../components/get-in-touch/get-in-touch.component';
+import { ContactSectionComponent } from '../../components/contact-section/contact-section.component';
+import { DreamProjectComponent } from '../../components/dream-project/dream-project.component';
 import { LightBrandsComponent } from '../../components/brands/light-brands/light-brands.component';
-import { PortfolioSectionComponent } from "../../components/portfolio-section/portfolio-section.component";
-import { PricingSectionComponent } from "../../components/pricing-section/pricing-section.component";
+import { PortfolioSectionComponent } from '../../components/portfolio-section/portfolio-section.component';
+import { PricingSectionComponent } from '../../components/pricing-section/pricing-section.component';
+import { FeaturesContentComponent as FeaturesContentComponent } from '../../components/features-section/features-content/features-content.component';
+import { FeaturesCardsComponent as FeaturesCardsComponent } from '../../components/features-section/features-cards/features-cards.component';
 
 @Component({
   selector: 'app-home',
@@ -31,8 +31,6 @@ import { PricingSectionComponent } from "../../components/pricing-section/pricin
     HeroSectionComponent,
     BannerSectionComponent,
     HowWorkComponent,
-    FeaturesContentComponent,
-    FeaturesCardsComponent,
     ServiceContentComponent,
     ServiceCardsComponent,
     GetStartedComponent,
@@ -50,8 +48,10 @@ import { PricingSectionComponent } from "../../components/pricing-section/pricin
     LightBrandsComponent,
     PortfolioSectionComponent,
     PortfolioSectionComponent,
-    PricingSectionComponent
-],
+    PricingSectionComponent,
+    FeaturesContentComponent,
+    FeaturesCardsComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -64,5 +64,4 @@ export class HomeComponent {
 
   description2: string =
     "Design, business understanding, ability to put themselves in the merchant's shoes meant to partner.";
-
-  }
+}
